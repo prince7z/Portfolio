c7c23482eada058f3f16c4ccc51890effc6c44ec
prince.7z <prince.cs231158@global.org.in>
2026-05-20 01:26:38 +0530

    refactor: remove smooth option from Lenis initialization, update tsconfig include paths, and add global CSS module declaration

--- app/components/SmoothScrollProvider.tsx
+++ app/components/SmoothScrollProvider.tsx
@@ -17,7 +17,6 @@ export default function SmoothScrollProvider({ children }: { children: React.Rea
         lenis = new Lenis({
           duration: 1.2,
           easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-          smooth: true,
         });
 
         const raf = (time: number) => {
