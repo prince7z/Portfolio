572e8f7468cc281998a1caf02ee8c1c42ace403e
prince.7z <prince.cs231158@global.org.in>
2026-05-20 18:57:22 +0530

    refactor: simplify copyright section and update year to 2026

--- app/sections/Copyright.tsx
+++ app/sections/Copyright.tsx
@@ -4,29 +4,10 @@ const Copyright = () => {
   return (
     <div className="  py-6  flex flex-col justify-center items-center font-medium text-xs sm:text-sm backdrop-blur-lg ">
       <p className=" text-center">
-        Designed by
-        <Link
-          href={"https://github.com/Farouk-ayo/devfarouk"}
-          className=" text-secondary-color-3   font-medium font-idgrotesk"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          devfarouk 👨‍💻
-        </Link>{" "}
-        (Inspirations from{" "}
-        <Link
-          href={"https://www.geekmaros.dev/"}
-          className=" font-medium"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Geekmaros
-        </Link>{" "}
-        )
+        Designed by devfarouk 
       </p>
       <div></div>
-      <p className=" ">&copy; 2024 All rights reserved.</p>
+      <p className=" ">&copy; 2026 All rights reserved.</p>
     </div>
   );
 };
